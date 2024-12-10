cc89c9e1ee0bbf22dd2a8831a1a6bf5828728dad
Judah40 <judahdore@gmail.com>
2024-12-10 13:30:33 +0000

    correction to last commit

--- src/controllers/favoriteController.js
+++ src/controllers/favoriteController.js
@@ -8,7 +8,7 @@ exports.handleCreateFavorites = async (req, res) => {
   const { id } = req.user;
 
   try {
-    const favoriteAlreadyExists = Favorite.find({
+    const favoriteAlreadyExists = Favorite.findOne({
       where: { id: id },
     });
     if (favoriteAlreadyExists) {
