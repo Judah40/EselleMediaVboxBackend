cd667d10d9483509142a0e50b2458be81a822133
unknown <judahdore@gmail.com>
2025-01-18 10:31:03 -0700

    update:video on demand comment setion added

--- src/routes/CommentRoute/userComment.js
+++ src/routes/CommentRoute/userComment.js
@@ -7,15 +7,31 @@ const {
   handleCreateCommentForVod,
   handleGetAllCommentsForVod,
 } = require("../../controllers/commentController");
-const { CommentValidator, vodCommentValidator } = require("../../utils/validators/commentValidator");
+const {
+  CommentValidator,
+  vodCommentValidator,
+} = require("../../utils/validators/commentValidator");
+const {
+  requireAuthenticatedUser,
+} = require("../../middlewares/auth.middleware");
 const router = express.Router();
 
 ////////////////////////////////////////////////////////////////
 //CREATE LIVE COMMENT
-router.post("/live", CommentValidator, handleCreateComment);
+router.post(
+  "/live",
+  CommentValidator,
+  requireAuthenticatedUser,
+  handleCreateComment
+);
 ////////////////////////////////////////////////////////////////
 //CREATE LIVE COMMENT FOR VOD
-router.post("/vod", vodCommentValidator, handleCreateCommentForVod);
+router.post(
+  "/vod",
+  vodCommentValidator,
+  requireAuthenticatedUser,
+  handleCreateCommentForVod
+);
 ////////////////////////////////////////////////////////////////
 //GET ALL LIVE COMMENTS FOR VOD
 router.get("/vod/:id", handleGetAllCommentsForVod);
@@ -24,9 +40,17 @@ router.get("/vod/:id", handleGetAllCommentsForVod);
 router.get("/live/:id", handleGetAllComments);
 ////////////////////////////////////////////////////////////////
 //DELETE SINGLE LIVE COMMENT
-router.delete("/live/:commentId", handleDeleteComment);
+router.delete(
+  "/live/:commentId",
+  requireAuthenticatedUser,
+  handleDeleteComment
+);
 ////////////////////////////////////////////////////////////////
 //DELETE SINGLE LIVE COMMENT FOR VOD
-router.delete("/vod/:commentId", handleDeleteCommentForVod);
+router.delete(
+  "/vod/:commentId",
+  requireAuthenticatedUser,
+  handleDeleteCommentForVod
+);
 
 module.exports = router;
